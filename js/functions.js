1df02a263b51cdd03069f2919f6226ad8c58bce2
sarhov <sarhov@sarhov.com>
2014-07-29 10:19:43 +0400

    end product tabs script commenting

--- js/functions.js
+++ js/functions.js
@@ -3,24 +3,28 @@ $(document).ready(function() {
 /*  --------------- */
 
 
-// product-tabs
+/*product-tabs*/
 
-
+	// initial values for tabs, before any action
 	$('.jsProductTabItem').first().addClass('active').siblings().removeClass('active');
 	$('.jsProductTabContentItem').first().fadeIn().siblings().hide();
 
-
+	// produtc tabs actions
 	$('.jsProductTabItem').click(function(e) {
 		e.preventDefault();
+		// check if we are in current tab, and no need to go ahead
 		if ($(this).hasClass('active')) {
 			return
 		}
+		// if not, we can switch tab
 		else{
 		$(this).addClass('active').siblings().removeClass('active');
 		var needTab = $(this).data('href');
 		$('.jsProductTabContentItem[data-name="'+ needTab +'"]').fadeIn().siblings().hide();
 	}
 	});
+
+/* END product tabs */
 
 
 
