bb346264acab420805e167e3b71815dd4fb5969d
sarhov <sarhov@sarhov.com>
2014-07-29 10:16:14 +0400

    end product tabs script

--- js/functions.js
+++ js/functions.js
@@ -1,4 +1,34 @@
 $(document).ready(function() {
     console.log('I\'m ready!');
+/*  --------------- */
 
+
+// product-tabs
+
+
+	$('.jsProductTabItem').first().addClass('active').siblings().removeClass('active');
+	$('.jsProductTabContentItem').first().fadeIn().siblings().hide();
+
+
+	$('.jsProductTabItem').click(function(e) {
+		e.preventDefault();
+		if ($(this).hasClass('active')) {
+			return
+		}
+		else{
+		$(this).addClass('active').siblings().removeClass('active');
+		var needTab = $(this).data('href');
+		$('.jsProductTabContentItem[data-name="'+ needTab +'"]').fadeIn().siblings().hide();
+	}
+	});
+
+
+
+
+
+
+
+
+
+/*  ----------------- */
 });
