75350405aee97811dda592f9a5f49e1e9aa95687
sarhov <sarhov@sarhov.com>
2014-08-04 07:12:44 +0400

    slider

--- js/functions.js
+++ js/functions.js
@@ -73,7 +73,61 @@ $(document).ready(function() {
 
 /* END Grid  - List */
 
+// http://docs.dev7studios.com/caroufredsel-old/examples/callback-functions.php
+// http://docs.dev7studios.com/caroufredsel-old/examples/custom-events.php
+// http://docs.dev7studios.com/caroufredsel-old/code-examples/custom-events.php
+$(function () {
+  $('#carousel').carouFredSel({
+    width: '100%',
+    align: 'center',
+	responsive: true,
+    // set active slide on first load
+    // (and when slides are dynamically added if I'm not mistaken)
+    onCreate : function( data ) {
+      data.items.filter(":eq(0)").addClass("active")
+       setTimeout(function() {data.items.filter(":eq(0)").removeClass('active'); }, 2700);
+    },
 
+    items: {
+      visible: 1,
+      //start: -1
+    },
+    scroll: {
+      //items: 3,
+      duration: 1000,
+      timeoutDuration: 5200,
+      fx: 'crossfade',
+
+      // set active slide on scroll
+      // onBefore : $current_position,
+      onBefore : function( data ) {
+        //data.items.old.removeClass( "active" ).delay(7000);
+        //timeoutDuration 	5 
+
+        data.items.old.filter(":eq(0)").removeClass('active');         console.log(data);
+      },
+      onAfter	: function( data ) {
+        //data.items.old.removeClass( "active" );
+        data.items.visible.filter(":eq(0)").addClass("active");      
+        setTimeout(function() {data.items.visible.filter(":eq(0)").removeClass('active'); }, 2700);
+      },
+
+      /*onAfter : $highlight,*/
+    },
+    prev : {
+      button : ".carousel-nav.prev",
+      key : "left"
+    },
+    next : {
+      button : ".carousel-nav.next",
+      key : "right"
+    },
+    pagination: {
+      container: '.carousel-pagination',
+      deviation: 0
+    }
+  });
+});
 
 
 /*  ----------------- */
